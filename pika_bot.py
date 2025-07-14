3a0624571313b6529c97fd211de146f618bb4296
lambipilled <shawtysmookz@gmail.com>
2025-07-14 00:42:30 -0700

    Update pika_bot.py

--- pika_bot.py
+++ pika_bot.py
@@ -6,8 +6,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from openai import OpenAI
-client = 
-OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 intents = discord.Intents.default()
 intents.message_content = True

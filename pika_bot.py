5a301fd697ca3f726f92bb6dae520c3c65681c8c
lambipilled <shawtysmookz@gmail.com>
2025-07-13 22:29:17 -0700

    Update pika_bot.py

--- pika_bot.py
+++ pika_bot.py
@@ -3,7 +3,6 @@ import random
 from discord.ext import commands, tasks
 import asyncio
 import os
-from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
